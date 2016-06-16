91058c4bbf9184ca343ce6f30609c7ffbf8f3d99
ColdNightHour <jdixo001@ucr.edu>
2016-06-16 11:45:33 -0700

    Got to detect if ethernet vs wifi available

--- wifi_script.sh
+++ wifi_script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Proper header for a Bash script.
 export PATH=$PATH:/home/pi/programmaticWifi
-sudo ifdown wlan0
-HELLO = iwlist wlan0 scan | grep SSID
-echo $HELLO
+#Checks the network links to a given computer 
+LINKS=$(ip link show | grep 'state UP')
+(echo $LINKS | grep "wlan0") && echo "Wifi available" || echo "Wifi unavailable"
+(echo $LINKS | grep "eth0") && echo "Ethernet available" || echo "Ethernet unavailable"
