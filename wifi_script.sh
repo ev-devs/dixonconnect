df91e09d6ced7637035978cc376c1913c09a8406
ColdNightHour <jdixo001@ucr.edu>
2016-06-16 15:00:02 -0700

    added flag checking and proper link configs

--- wifi_script.sh
+++ wifi_script.sh
@@ -3,13 +3,25 @@
 export PATH=$PATH:/home/pi/programmaticWifi
 #Checks the network links to a given computer 
 LINKS=$(ip link show | grep 'state UP')
+#Defines and sets flags for eth0 and wlan0
 WFLAG="false"
-#Checks if the WLAN is available and if so there there are possible wifi AP's to connect to
-(echo $LINKS | grep "wlan0") && printf "\nWifi available\n" ; WFLAG="true" || printf "\nWifi unavailable\n"
-#Check if there is a connection on eth0 (first ethernet face) and if so it will connect automatically to said ethernet face
-(echo $LINKS | grep "eth0") && printf "\nEthernet available\n" || printf "\nEthernet unavailable\n"
+EFLAG="false"
 
-if["true" = "true"]; then
+#If the wlan0 is available then set the flag to true
+WLAN0=$(echo $LINKS | grep "wlan0")
+if [ "$WLAN0" != " " ] 
+then
+  WFLAG="true"
+fi
+
+ETH0=$(echo $LINKS | grep "eth0")
+if [ "$ETH0" != " " ] 
+then
+  EFLAG="true"
+fi
+
+if [ $WFLAG  != "true" ]
+then
    #Scans the WLAN0 and gets only the WIFI name with the ESSID. Then the ESSID is taken away with sed
    W=$(sudo iwlist wlan0 scan | grep ESSID | sed -e 's/\<ESSID\>//g') 
    WIFICONS=$(echo $W)
