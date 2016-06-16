2932cc7aa45cb55fa01e186afcc4aba084b55f75
ColdNightHour <jdixo001@ucr.edu>
2016-06-16 13:40:08 -0700

    got to check ethernet/wifi and display all wifi signals

--- wifi_script.sh
+++ wifi_script.sh
@@ -3,5 +3,19 @@
 export PATH=$PATH:/home/pi/programmaticWifi
 #Checks the network links to a given computer 
 LINKS=$(ip link show | grep 'state UP')
-(echo $LINKS | grep "wlan0") && echo "Wifi available" || echo "Wifi unavailable"
-(echo $LINKS | grep "eth0") && echo "Ethernet available" || echo "Ethernet unavailable"
+WFLAG="false"
+#Checks if the WLAN is available and if so there there are possible wifi AP's to connect to
+(echo $LINKS | grep "wlan0") && printf "\nWifi available\n" ; WFLAG="true" || printf "\nWifi unavailable\n"
+#Check if there is a connection on eth0 (first ethernet face) and if so it will connect automatically to said ethernet face
+(echo $LINKS | grep "eth0") && printf "\nEthernet available\n" || printf "\nEthernet unavailable\n"
+
+if["true" = "true"]; then
+   #Scans the WLAN0 and gets only the WIFI name with the ESSID. Then the ESSID is taken away with sed
+   W=$(sudo iwlist wlan0 scan | grep ESSID | sed -e 's/\<ESSID\>//g') 
+   WIFICONS=$(echo $W)
+   IFS=':' read -r -a array <<< "$WIFICONS"
+   for element in "${array[@]}"
+   do
+      echo "$element"
+   done
+fi
