b6275a34a372af90a1fec2ef7588d0927667409f
ColdNightHour <jdixo001@ucr.edu>
2016-07-06 09:04:26 -0700

    Created a new script to remove Wi-Fi connection

---
+++ wifi_rem.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+FILE=$(sudo cat /etc/wpa_supplicant/wpa_supplicant.conf)
+#Replaces the newlines of the output FILE var and gets rid of everything after and including network
+PREVCONTENT=$( echo "$FILE" | tr '\n' '~' | sed 's/network.*//' )
+#Writes the new contents of the final
+#replaces the ~ with \n
+PREVCONTENT=$(echo $PREVCONTENT | tr '~' '\n')
+#Outputs to the file thus changes the configurations
+printf "$PREVCONTENT" > /etc/wpa_supplicant/wpa_supplicant.conf
+ifdown wlan0
+ifup wlan0
